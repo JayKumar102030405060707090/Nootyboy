0b78fc820d6a3ebab81dd184aa0f292701186143
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2024-05-19 00:01:55 +0530

    Update help.py

--- Bikash/utils/inline/help.py
+++ Bikash/utils/inline/help.py
@@ -13,11 +13,11 @@ def help_pannel(_, START: Union[bool, int] = None):
         InlineKeyboardButton(
             # text=_["BACK_BUTTON"],
             text="Updates",
-            url=f"https://t.me/BikashGadgetsTech",
+            url=f"https://t.me/LOVE_CONNECTIONN",
         ),
         InlineKeyboardButton(
             text="Creator",
-            url=f"t.me/BikashHalder",
+            url=f"t.me/ATTITUDE_MAXXXX",
         ),
         InlineKeyboardButton(
             text="Close", callback_data=f"close"
